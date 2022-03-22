119bb9f633641e57eac0c8563bedf960830a6c28
JavBurgoa <javierburgoac@gmail.com>
2022-03-22 17:13:46 +0100

    [datasets].js pages now have a proper switchable information panel

--- pages/[dataset].js
+++ pages/[dataset].js
@@ -151,6 +151,7 @@ export const getStaticProps = async (context) => {
 
     // Functions for HTML
     const collapse = (id) => {
+        // Obsolete
         // applied to a button it collapses whatever DOM element by id
 
         var panel = document.getElementById(id)
@@ -162,32 +163,48 @@ export const getStaticProps = async (context) => {
 
     }
 
-    const paperOrAchive= (metaData) => {
-        // Outputs metaData.biorxiv if there is no paper_url
+    const  metaDataTop = (metaData, top, second) => {
+        // Outputs metaData.second if there is no metaData.top
 
-        if(metaData.paper_url === ""){
-            var text = metaData.biorxiv_url
-        }else{var text = metaData.paper_url}
+        if(metaData[top] === ""){
+            var text = metaData[second]
+        }else{var text = metaData[top]}
         
         return(text)
 
     }
 
 
-    // Functions related to swithable tab
-    const setupTabs = (data_number) => {
+
+    const switchTab = (to) => {
         // This function goes in an onclick event
         // When clicked on it it will turn one ofthte sidebar buttons active aswell as one of the contents
-        // Whihc one is activated is chosen with the data_number argument that decides on the data-for-tab and data-tab tags on the html
-        // that is going to be activated
+        // Which one is activated is chosen with the "to" argument whihc should be a number from 1 to 3
+        
+        
+        // Remove active class from active buton/content
+        var dataIds = [1,2,3]
+        dataIds.splice(to-1, 1)
 
-        const buttons = document.getElementById("sidebar").childNodes
+        dataIds.forEach((id)=>{
+            const activeButton = document.getElementById("data_tab_" + id.toString())
+            const activeContent = document.getElementById("data_content_" + id.toString())
 
-        buttons.forEach( (e) => 
-                {
-                    console.log(e.className)
-                }
-            )
+            console.log("aaaaaaaaaa!!!!!!!!!!")
+            console.log(activeContent)
+            
+            activeButton.className = style.tab_btn
+            activeContent.className = style.tab_content
+        })
+
+        // Change selected divs to active
+        const tabBarName = "data_tab_" + to.toString()
+        const tabContentName = "data_content_" + to.toString()
+        const tabBar = document.getElementById(tabBarName)
+        const tabContent = document.getElementById(tabContentName)
+
+        tabBar.className = style.tab_btn_active // button
+        tabContent.className = style.tab_content_active // content        
     }
 
 
@@ -198,55 +215,40 @@ const Details = ({metaData}) =>{
         
         <div>
             <iframe src="https://cells-test.gi.ucsc.edu/?ds=evocell+clyhem" className={style.UCSCiframe} title="UCSC Cell Browser" alt = "UCSC Cell Browser"></iframe>
-            <div>
-                <p className={style.descriptionTitle}>Paper</p>
-                <p className={style.description}>{metaData.title}</p>
-            </div>
-            <div>
-                <button id="abstractButton" className="collapseButton" onClick={(e) => collapse("abstractDesc")}>Abstract</button>
-                <p id = "abstractDesc" className={style.description}>{metaData.abstract}</p>
-            </div>
-            <div>
-            <button id="abstractButton" className="collapseButton" onClick={(e) => collapse("methodsDesc")}>Methods</button>
-                <p id = "methodsDesc" className={style.description}>{metaData.methods}</p>
-            </div>
-            <div>
-                <p className={style.description}>Paper url: {paperOrAchive(metaData)}</p>
-                <p className={style.description}>doi: {metaData.doi}</p>
-                <p className={style.description}>GEO: {metaData.geo_series}</p>
-                <p className={style.description}>Author&apos;s institution: {metaData.institution}</p>
-                <p className={style.description}>Author: {metaData.author}</p>
-                <p className={style.description}>Lab: {metaData.lab}</p>
-                <p className={style.description}>Ontogenic stage: {metaData.custom.ontogenic_stage}</p>
-                <p className={style.description}>Specialised resources: {metaData.custom.more_specialised_resources}</p>
-                <p className={style.description}>Notes: {metaData.custom.notes}</p>
-                <p className={style.description}>Genome: {metaData.custom.genome}</p>
-                <p className={style.description}>Transcriptome: {metaData.custom.transcriptome}</p>
-                <p className={style.description}>Tissue: {metaData.custom.tissue_type}</p>
-                <p className={style.description}>Number of Cells: {metaData.custom.number_of_cells}</p>
-                <p className={style.description}>Sequencing: {metaData.custom.sequencing_method}</p>
-            </div>
 
 
             <div className={style.tabs}>
 
                 <div id="sidebar" className={style.sidebar}>
-                    <button className={`${style.tab_btn} ${style.tab_btn_active}`} data-for-tab="1" onClick={(e) => setupTabs()}>Publication</button>
-                    <button className={style.tab_btn} data-for-tab="2">Methods</button>
-                    <button className={style.tab_btn} data-for-tab="3">Data availability</button>
+                    <button className={style.tab_btn_active} id = "data_tab_1" onClick={() => switchTab(1)}>Publication</button>
+                    <button className={style.tab_btn} id = "data_tab_2" onClick={() => switchTab(2)}>Methods</button>
+                    <button className={style.tab_btn} id = "data_tab_3" onClick={() => switchTab(3)}>Dataset</button>
                 </div>
                 <div className={style.content}>
-                    <div className={`${style.tab_content} ${style.tab_content_active}`} data-tab="1">
-                        <p className={style.description}>{metaData.title}</p>
-                        <p className={style.description}>{metaData.abstract}</p>
-                        <p className={style.description}>Paper url: {paperOrAchive(metaData)}</p>
-                        <p className={style.description}>doi: {metaData.doi}</p>
-                        <p className={style.description}>Author&apos;s institution: {metaData.institution}</p>
-                        <p className={style.description}>Author: {metaData.author}</p>
-                        <p className={style.description}>Lab: {metaData.lab}</p>
+                    <div className={style.tab_content_active} id = "data_content_1">
+                        <p className={style.description}><span>Title: </span><a href = {metaDataTop(metaData, "paper_url", "biorxiv_url")}>{metaData.title}</a></p>
+                        <p className={style.description}><span>Abstract: </span> <br></br> {metaData.abstract}</p>
+                        <p className={style.description}><span>Doi: </span> {metaData.doi}</p>
+                        <p className={style.description}><span>Author&apos;s institution: </span> {metaData.institution}</p>
+                        <p className={style.description}><span>Author: </span> {metaData.author}</p>
+                        <p className={style.description}><span>Lab: </span> {metaData.lab}</p>
                     </div>
-                    <div className={style.tab_content} data-tab="2">Method man</div>
-                    <div className={style.tab_content} data-tab="3">Data stuff</div>
+                    <div className={style.tab_content} id = "data_content_2">
+                        <p className={style.description}><span>Methods summary: </span><br></br>{metaData.methods}</p>
+                        <p className={style.description}><span>Sequencing: </span>{metaData.custom.sequencing_method}</p>
+                        <p className={style.description}><span>Genome: </span>{metaData.custom.genome}</p>
+                        <p className={style.description}><span>Transcriptome: </span>{metaData.custom.transcriptome}</p>
+                        <p className={style.description}><span>Notes: </span>{metaData.custom.notes}</p>
+                    </div>
+                    <div className={style.tab_content} id = "data_content_3">
+                        <p className={style.description}><span>Ontogenic stage: </span>{metaData.custom.ontogenic_stage}</p>
+                        <p className={style.description}><span>Tissue: </span>{metaData.custom.tissue_type}</p>
+                        <p className={style.description}><span>Number of cells: </span>{metaData.custom.number_of_cells}</p>
+                        <p className={style.description}><span>GEO: </span>{metaData.geo_series}</p>
+                        <p className={style.description}><span>SRA: </span>{metaData.sra_study}</p>
+                        <p className={style.description}><span>Specialised resources: </span>{metaData.custom.more_specialised_resources}</p>
+                        <p className={style.description}><span>Submitter: </span>{metaData.submitter}</p>
+                    </div>
                 </div>
 
             </div>
